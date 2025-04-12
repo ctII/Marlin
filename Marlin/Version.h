3deb398b74f1297853cbff3c4c2efd4a01b87fdf
Clayton Townsend II <clayton@claytontii.com>
2025-04-12 14:31:18 -0500

    update url for source code in Version.h

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -61,7 +61,7 @@
  * has a distinct Github fork— the Source Code URL should just be the main
  * Marlin repository.
  */
-//#define SOURCE_CODE_URL "github.com/MarlinFirmware/Marlin"
+#define SOURCE_CODE_URL "github.com/ctII/Marlin"
 
 /**
  * Default generic printer UUID.
